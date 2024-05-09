8273e8fd9bc7b038575f6a36528a2f26702d80d7
Chris Laliberte <chris.laliberte@BCFEDU.local>
2024-05-09 11:22:26 -0400

    Implement hello_world() function

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest 
 from hello import hello_world
 from io import StringIO
 from unittest.mock import patch
@@ -7,4 +7,3 @@ def test_hello_world_output():
     with patch('sys.stdout', new=StringIO()) as fake_out:
         hello_world()
         assert fake_out.getvalue().strip() == "Hello World!"
-        
